ee8d19ec662ef2dcbcc08e0871d2df11846f65a4
Developer11 <Developer1@gmail.com>
2021-07-26 12:53:48 +0530

    Intial commit by TL

---
+++ pragyaproject/src/com/pragya/beans/utility.java
@@ -0,0 +1,11 @@
+package com.pragya.beans;
+
+import java.util.Date;
+
+public class utility {
+	
+	public  Date getSystemDate(){
+		return new Date();                      //ctrl+shift+o
+		
+	}
+}
